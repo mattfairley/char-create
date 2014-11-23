f357925cefa27246440f10041d564f89c99f2e76
mattfairley <matt.fairley@gmail.com>
2014-11-23 16:13:16 -0500

    First commit - workflow tooling done

---
+++ src/js/scripts.js
@@ -0,0 +1,9 @@
+(function() {
+  var app = angular.module('charCreate', []);
+
+  app.controller('CharController', function(){
+    var character = this;
+    character.stats = [];
+    
+  });
+})();
