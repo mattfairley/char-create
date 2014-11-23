d2a87ffe04821f2f2dbb29ece237e8c85aaa4b4a
mattfairley <matt.fairley@gmail.com>
2014-11-23 16:44:18 -0500

    finished setting up templating shells for form customization

--- dist/assets/js/scripts.js
+++ dist/assets/js/scripts.js
@@ -6,4 +6,80 @@
     character.stats = [];
     
   });
+
+  app.directive('statTabs', function() {
+    return {
+      restrict: 'E',
+      templateUrl: 'stat-tabs.html',
+      controller: function() {
+        this.tab = 1;
+
+        this.isSet = function(checkTab) {
+          return this.tab === checkTab;
+        };
+
+        this.setTab = function(activeTab) {
+          this.tab = activeTab;
+        };
+      },
+      controllerAs: 'tab'
+    };
+  });
+
+  app.directive('statName', function(){
+    return {
+      restrict: 'E',
+      templateUrl: 'stat-name.html'
+    };
+  });
+
+  app.directive('statRace', function(){
+    return {
+      restrict: 'E',
+      templateUrl: 'stat-race.html'
+    };
+  });
+
+  app.directive('statClass', function(){
+    return {
+      restrict: 'E',
+      templateUrl: 'stat-class.html'
+    };
+  });
+
+  app.directive('statAttributes', function(){
+    return {
+      restrict: 'E',
+      templateUrl: 'stat-attributes.html'
+    };
+  });
+
+  app.directive('statSkills', function(){
+    return {
+      restrict: 'E',
+      templateUrl: 'stat-skills.html'
+    };
+  });
+
+  app.directive('statSpells', function(){
+    return {
+      restrict: 'E',
+      templateUrl: 'stat-spells.html'
+    };
+  });
+
+  app.directive('statWeapons', function(){
+    return {
+      restrict: 'E',
+      templateUrl: 'stat-weapons.html'
+    };
+  });
+
+  app.directive('statSheet', function(){
+    return {
+      restrict: 'E',
+      templateUrl: 'stat-sheet.html'
+    };
+  });
+
 })();
